490122476811ca0e25a9dfe9c6b3533b3bdce880
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-19 00:51:49 +0200

    add carousel

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,2 +1,26 @@
-// import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
-// import '@fancyapps/ui/dist/fancybox.css';
+import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox.css';
+// Initialise Carousel
+// Initialise Carousel
+const myCarousel = new Carousel(document.querySelector('#myCarousel'), {
+  preload: 2,
+});
+
+// Customize Fancybox
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: false,
+  Toolbar: false,
+
+  closeButton: 'top',
+
+  Carousel: {
+    Dots: true,
+    on: {
+      change: that => {
+        myCarousel.slideTo(myCarousel.findPageForSlide(that.page), {
+          friction: 0,
+        });
+      },
+    },
+  },
+});
