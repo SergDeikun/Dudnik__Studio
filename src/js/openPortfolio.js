bfaf1bd679d7e4d40770c08fa9c8aaf0d3b6a0c5
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-23 20:50:36 +0200

    fix social list styles

--- src/js/openPortfolio.js
+++ src/js/openPortfolio.js
@@ -7,15 +7,25 @@
     propertiesSection: document.querySelector('.js-properties'),
     aboutSection: document.querySelector('.js-about'),
     sliderSection: document.querySelector('.js-slider'),
+    aboutHeder: document.querySelector('.js-about-heder'),
   };
 
   refs.portfolioLink?.addEventListener('click', toggleMenu);
+  refs.aboutHeder?.addEventListener('click', back);
 
   function toggleMenu() {
-    refs.heroSection.classList.add('is-hidden');
-    refs.propertiesSection.classList.add('is-hidden');
-    refs.aboutSection.classList.add('is-hidden');
-    refs.sliderSection.classList.add('is-hidden');
+    refs.heroSection?.classList.add('is-hidden');
+    refs.propertiesSection?.classList.add('is-hidden');
+    refs.aboutSection?.classList.add('is-hidden');
+    refs.sliderSection?.classList.add('is-hidden');
     refs.portfolioSection?.classList.remove('is-hidden');
+  }
+
+  function back() {
+    refs.heroSection?.classList.remove('is-hidden');
+    refs.propertiesSection?.classList.remove('is-hidden');
+    refs.aboutSection?.classList.remove('is-hidden');
+    refs.sliderSection?.classList.remove('is-hidden');
+    refs.portfolioSection?.classList.add('is-hidden');
   }
 })();
