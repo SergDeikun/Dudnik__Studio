c93a9265231520abc6b1ce3b379e0aaec4cbb6d7
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-26 00:04:11 +0200

    update mobMenu

--- src/js/openPortfolio.js
+++ src/js/openPortfolio.js
@@ -1,19 +1,22 @@
 (() => {
   const refs = {
-    portfolioLink: document.querySelector('.js-open-portfolio'),
-    firstPage: document.querySelector('.first-page'),
+    portfolioLinkHeadr: document.querySelector('.js-open-portfolio'),
+    portfolioLinkMobMenu: document.querySelector('.js-portfolio'),
     portfolioSection: document.querySelector('.js-portfolio-section'),
     heroSection: document.querySelector('.js-hero'),
     propertiesSection: document.querySelector('.js-properties'),
     aboutSection: document.querySelector('.js-about'),
     sliderSection: document.querySelector('.js-slider'),
-    aboutHeder: document.querySelector('.js-about-heder'),
+    aboutUsHeaderLink: document.querySelector('.js-about-header'),
+    aboutUsMobMenuLink: document.querySelector('.js-about-us'),
   };
 
-  refs.portfolioLink?.addEventListener('click', toggleMenu);
-  refs.aboutHeder?.addEventListener('click', back);
+  refs.portfolioLinkHeadr?.addEventListener('click', openPortfolio);
+  refs.portfolioLinkMobMenu?.addEventListener('click', openPortfolio);
+  refs.aboutUsHeaderLink?.addEventListener('click', back);
+  refs.aboutUsMobMenuLink?.addEventListener('click', back);
 
-  function toggleMenu() {
+  function openPortfolio() {
     refs.heroSection?.classList.add('is-hidden');
     refs.propertiesSection?.classList.add('is-hidden');
     refs.aboutSection?.classList.add('is-hidden');
