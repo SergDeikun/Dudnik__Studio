320b044cc0949c912ff8e0e130b8de72fdc7b2ce
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-25 02:12:57 +0200

    change about section

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,6 +1,2 @@
 import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox.css';
-// Initialise Carousel
-const myCarousel = new Carousel(document.querySelector('#myCarousel'), {
-  preload: 2,
-});
