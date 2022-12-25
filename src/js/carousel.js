65839bfe58e63f204f372ec2ae50ea5376bc32e7
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-25 22:08:38 +0200

    fix about section

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,6 +1,6 @@
-import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox.css';
-// Initialise Carousel
-const cardSlider = new Carousel(document.querySelector('#cardSlider'), {
-  Dots: false,
-});
+// import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox.css';
+// // Initialise Carousel
+// const cardSlider = new Carousel(document.querySelector('#cardSlider'), {
+//   Dots: false,
+// });
