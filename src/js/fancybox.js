63204b6cd1bfbb501b7c4493fe5850ea9e8e5e4a
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-27 23:53:21 +0200

    change fansybox

--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,5 +1,12 @@
 import { Fancybox } from '@fancyapps/ui/src/Fancybox/Fancybox.js';
 
-Fancybox.bind('[data-fancybox="single"]', {
-  groupAttr: false,
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: false,
+  Toolbar: false,
+
+  Image: {
+    zoom: false,
+    click: false,
+    wheel: 'slide',
+  },
 });
