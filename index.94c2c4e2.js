5d7f684e7d33d7a23ef11a54b95cad7977eb0b9e
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-12-27 20:40:04 +0000

    Deploying to gh-pages from @ SergDeikun/Dudnik__Studio@3239322bfb1a4b86b195af66734bd7fa24d62c11 🚀

--- index.94c2c4e2.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={mobMenu:document.querySelector(".js-menu-container"),openMenuBtn:document.querySelector(".js-open-menu"),closeMenuBtn:document.querySelector(".js-close-menu")};function n(){e.mobMenu?.classList.toggle("is-hidden")}e.openMenuBtn?.addEventListener("click",n),e.closeMenuBtn?.addEventListener("click",n)})();
-//# sourceMappingURL=index.94c2c4e2.js.map
